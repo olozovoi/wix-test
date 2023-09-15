fa48aaf6e39bd4ce3ea9664596a61a62e47d6033
Lozovoi Oleksii <aleksej.lozovoy@gmail.com>
2023-09-15 05:08:45 +0300

    scalafmt

--- engine/src/main/scala/core/services/generators/ConsecutiveGenerator.scala
+++ engine/src/main/scala/core/services/generators/ConsecutiveGenerator.scala
@@ -6,6 +6,6 @@ import zio.*
 
 object ConsecutiveGenerator extends Generator {
   override def gen: UIO[Game] = ZIO.succeed(Game(Board((0 to 15).toVector), 0))
-  
+
   val layer: ULayer[Generator] = ZLayer.succeed(ConsecutiveGenerator)
 }
