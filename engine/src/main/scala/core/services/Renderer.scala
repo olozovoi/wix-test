936fc7c4eab3af3de481a1501d34239c85a73eee
Lozovoi Oleksii <aleksej.lozovoy@gmail.com>
2023-09-15 04:07:44 +0300

    scalafmt

--- engine/src/main/scala/core/services/Renderer.scala
+++ engine/src/main/scala/core/services/Renderer.scala
@@ -5,12 +5,12 @@ import zio.*
 
 trait Renderer {
   def render(game: Game): UIO[Unit]
-  
+
   def showText(text: String): UIO[Unit]
 }
 
 object Renderer {
   def render(game: Game): URIO[Renderer, Unit] = ZIO.serviceWithZIO(_.render(game))
-  
+
   def showText(text: String): URIO[Renderer, Unit] = ZIO.serviceWithZIO(_.showText(text))
 }
