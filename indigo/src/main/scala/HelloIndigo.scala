0f1e6df7fcb5af7ee6dd28cd6291224fc00b3b04
Lozovoi Oleksii <aleksej.lozovoy@gmail.com>
2023-09-18 00:23:57 +0300

    move HelloIndigo to IndigoGame subclass

--- indigo/src/main/scala/HelloIndigo.scala
+++ indigo/src/main/scala/HelloIndigo.scala
@@ -1,26 +1,38 @@
 import indigo.*
+import indigo.scenes.*
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("IndigoGame")
-object HelloIndigo extends IndigoSandbox[Unit, Model] {
+object HelloIndigo extends IndigoGame[Unit, Unit, Model, Unit] {
 
   private val magnification = 3
 
   val config: GameConfig = GameConfig.default.withMagnification(magnification)
 
-  val animations: Set[Animation] = Set()
-
   private val assetName = AssetName("dots")
 
   val assets: Set[AssetType] = Set(AssetType.Image(AssetName("dots"), AssetPath("assets/dots.png")))
 
-  val fonts: Set[FontInfo] = Set()
+  def initialScene(bootData: Unit): Option[SceneName] = None
 
-  val shaders: Set[Shader] = Set()
+  def scenes(bootData: Unit): NonEmptyList[Scene[Unit, Model, Unit]] = NonEmptyList(Scene.empty)
 
-  def setup(assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Unit]] =
+  def eventFilters: EventFilters = EventFilters.Permissive
+
+  def boot(flags: Map[String, String]): Outcome[BootResult[Unit]] =
+    Outcome(BootResult.noData(config).withAssets(assets))
+
+  def setup(bootData: Unit, assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Unit]] =
     Outcome(Startup.Success(()))
+
+  def initialViewModel(startupData: Unit, model: Model): Outcome[Unit] = Outcome(())
+
+  def updateViewModel(
+      context: FrameContext[Unit],
+      model: Model,
+      viewModel: Unit
+  ): GlobalEvent => Outcome[Unit] = _ => Outcome(())
 
   def initialModel(startupData: Unit): Outcome[Model] =
     Outcome(Model.initial(config.viewport.giveDimensions(magnification).center))
@@ -39,12 +51,14 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
     case _ => Outcome(model)
   }
 
-  def present(context: FrameContext[Unit], model: Model): Outcome[SceneUpdateFragment] = Outcome(
-    SceneUpdateFragment(
-      Graphic(Rectangle(0, 0, 32, 32), 1, Material.Bitmap(assetName)) ::
-        drawDots(model.center, model.dots)
-    )
-  )
+  def present(
+      context: FrameContext[Unit],
+      model: Model,
+      viewModel: Unit
+  ): Outcome[SceneUpdateFragment] = Outcome(SceneUpdateFragment(
+    Graphic(Rectangle(0, 0, 32, 32), 1, Material.Bitmap(assetName)) ::
+      drawDots(model.center, model.dots)
+  ))
 
   private def drawDots(center: Point, dots: Batch[Dot]): Batch[Graphic[_]] = dots.map { dot =>
     val position = Point(
