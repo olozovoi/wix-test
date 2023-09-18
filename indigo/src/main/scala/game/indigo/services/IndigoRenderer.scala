e6be4938dd8ba9b0ad5adf4d1c145f8298bc6a47
Lozovoi Oleksii <aleksej.lozovoy@gmail.com>
2023-09-18 17:48:34 +0300

    `IndigoRenderer#showText` as console.log for debugging

--- indigo/src/main/scala/game/indigo/services/IndigoRenderer.scala
+++ indigo/src/main/scala/game/indigo/services/IndigoRenderer.scala
@@ -5,13 +5,13 @@ import core.services.Renderer
 import game.indigo.subsystem.GameSubSystem.RenderEvent
 import game.indigo.subsystem.GameSubSystem.RenderEvent.{Render, ShowText}
 import indigo.shared.events.GlobalEvent
-import zio.{Queue, UIO, ULayer, ZIO, ZLayer}
+import zio.{Queue, UIO, ULayer, ZIO, ZLayer, Console}
 
 case class IndigoRenderer(renderQueue: Queue[RenderEvent]) extends Renderer {
   override def render(game: Game): UIO[Unit] = renderQueue.offer(Render(game)).ignore
 
   override def showText(text: String): UIO[Unit] =
-    ZIO.unit // renderQueue.offer(ShowText(text)).ignore
+    Console.printLine(text).orDie // renderQueue.offer(ShowText(text)).ignore
 }
 
 object IndigoRenderer {
